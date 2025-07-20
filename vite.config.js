33f35391140b02057f4ae2cba88a705dbb9a1e79
Darsha Anuradha <darsha.anuradha2020@gmail.com>
2025-07-20 20:07:13 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,17 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+  ],
+  build: {
+    manifest: true,
+    outDir: 'public/build',
+    rollupOptions: {
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+    },
+  },
 });
